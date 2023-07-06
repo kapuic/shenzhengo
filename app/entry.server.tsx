3216eb0efdc062bcc0528eda4a0d844625ca624c
Kapui Cheung <git@kapui.net>
2023-07-06 17:26:47 +0800

    fix: fix content security policy

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -41,8 +41,8 @@ export default async function handleRequest(
   // Security Headers
   responseHeaders.set(
     "Content-Security-Policy",
-    `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://webapi.amap.com/maps https://webapi.amap.com/mapsplugin https://webapi.amap.com/style2; style-src 'self' 'unsafe-inline'; img-src 'self' http://webapi.amap.com https://vdata.amap.com https://*.is.autonavi.com/appmaptile; frame-src https://challenges.cloudflare.com; frame-ancestors 'none'; worker-src blob:${
-      process.env.NODE_ENV === "development" ? "; connect-src *" : ""
+    `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://features-control.kapui.net https://analytics.kapui.net https://challenges.cloudflare.com https://webapi.amap.com/maps https://webapi.amap.com/mapsplugin https://webapi.amap.com/style2; style-src 'self' 'unsafe-inline'; img-src 'self' http://webapi.amap.com https://vdata.amap.com https://*.is.autonavi.com/appmaptile; frame-src https://challenges.cloudflare.com; frame-ancestors 'none'; worker-src blob:; connect-src https://*.amap.com/nebula/v3${
+      process.env.NODE_ENV === "development" ? " *" : ""
     }`
   );
   responseHeaders.set("X-Frame-Options", "DENY");
