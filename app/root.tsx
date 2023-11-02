943140f7f1f13e6c0abdf2607fc153854357d333
Kapui Cheung <git@kapui.net>
2023-11-02 12:13:20 +0800

    feat: add favicons

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,7 @@ import {
   json,
   type LinksFunction,
   type LoaderFunctionArgs,
+  type MetaFunction,
 } from "@remix-run/cloudflare";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import {
@@ -20,35 +21,30 @@ import { useEffect, useMemo } from "react";
 
 import tailwind from "~/tailwind.css";
 
+export const meta: MetaFunction = () => [
+  { name: "theme-color", content: "#1659be" },
+  { name: "msapplication-TileColor", content: "#1659be" },
+  { name: "msapplication-config", content: "/icons/browserconfig.xml" },
+];
+
 export const links: LinksFunction = () => [
+  ...["16", "32"].map((size) => ({
+    rel: "icon",
+    href: `/icons/favicon-${size}x${size}.png`,
+    sizes: `${size}x${size}`,
+    type: "image/png",
+  })),
+  { rel: "mask-icon", href: "/icons/safari-pinned-tab.svg", color: "#1659be" },
+  ...["60", "76", "120", "152", "180"].map((size) => ({
+    rel: "apple-touch-icon",
+    href: `/icons/apple-touch-icon-${size}x${size}.png`,
+    sizes: `${size}x${size}`,
+  })),
+  { rel: "manifest", href: "/icons/site.webmanifest" },
+  { rel: "shortcut icon", href: "/icons/favicon.ico" },
+
   { rel: "stylesheet", href: tailwind },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-  // { rel: "icon", href: "/icons/favicon.ico", type: "image/png" },
-  // { rel: "icon", href: "/icons/favicon.svg", type: "image/svg+xml" },
-  // {
-  //   rel: "icon",
-  //   href: "/icons/favicon-16x16.png",
-  //   sizes: "16x16",
-  //   type: "image/png",
-  // },
-  // {
-  //   rel: "icon",
-  //   href: "/icons/favicon-32x32.png",
-  //   sizes: "32x32",
-  //   type: "image/png",
-  // },
-  // {
-  //   rel: "mask-icon",
-  //   href: "/icons/mask-icon.svg",
-  //   color: "#1e90ff",
-  //   type: "image/svg+xml",
-  // },
-  // ...["60", "76", "120", "152", "180"].map((size) => ({
-  //   rel: "apple-touch-icon",
-  //   href: `/icons/apple-touch-icon-${size}x${size}.png`,
-  //   sizes: `${size}x${size}`,
-  //   type: "image/png",
-  // })),
 ];
 
 export async function loader({ context }: LoaderFunctionArgs) {
