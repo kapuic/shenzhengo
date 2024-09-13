6328f144446ecbb0ca92e2d197a1358e8f77633e
Kapui Cheung <git@kapui.net>
2024-09-13 00:45:55 -0400

    feat: update configs
    - Enable future flags.
    - Remove comments in build output.
    - Include `.client` and `.server`.

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,16 @@ export default defineConfig({
       routes: async (defineRoutes) => {
         return flatRoutes("routes", defineRoutes);
       },
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        unstable_singleFetch: true,
+      },
     }),
     tsconfigPaths(),
   ],
+  esbuild: { legalComments: "none" },
   ssr: {
     optimizeDeps: {
       include: ["lodash"],
