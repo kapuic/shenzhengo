0a5dc858c6ed52af470b3058e8ff4c488e28d6e5
Kapui Cheung <git@kapui.net>
2024-09-21 02:05:35 -0400

    feat(app): make `MapWelcomeMessage` responsive

--- app/routes/_app/_index/MapWelcomeMessage.tsx
+++ app/routes/_app/_index/MapWelcomeMessage.tsx
@@ -1,6 +1,12 @@
-import { IconClick, IconMapPin, IconMouse } from "@tabler/icons-react";
+import {
+  IconArrowsMove,
+  IconClick,
+  IconHandClick,
+  IconMapPin,
+} from "@tabler/icons-react";
+import { useHydrated } from "remix-utils/use-hydrated";
 import { twMerge } from "tailwind-merge";
-import { useLocalStorage } from "usehooks-ts";
+import { useLocalStorage, useMediaQuery } from "usehooks-ts";
 
 import Alert, { type AlertProps } from "~/components/Alert";
 import Button from "~/components/Button";
@@ -10,6 +16,8 @@ export default function MapWelcomeMessage({ className, ...props }: AlertProps) {
     "map.welcomeMessageDismissed",
     false,
   );
+  const hydrated = useHydrated();
+  const isTouchScreen = useMediaQuery("(hover: none), (pointer: coarse)");
 
   return (
     <Alert
@@ -17,22 +25,45 @@ export default function MapWelcomeMessage({ className, ...props }: AlertProps) {
       variant="dark"
       {...props}
     >
-      <ul className="flex flex-col gap-2 text-sm">
-        <li className="flex gap-2">
-          <IconMouse className="flex-shrink-0" />
-          <p>Drag and scroll or use your keyboard to navigate the map.</p>
-        </li>
-        <li className="flex gap-2">
-          <IconMapPin className="flex-shrink-0" />
-          <p>Click a pin to learn more about that place.</p>
-        </li>
-        <li className="flex gap-2">
-          <IconClick className="flex-shrink-0" />
-          <p>
-            Click a card from the list below to focus on that place on the map.
-          </p>
-        </li>
-      </ul>
+      {hydrated && isTouchScreen ? (
+        <ul className="flex flex-col gap-2 text-sm">
+          <li className="flex gap-2">
+            <IconArrowsMove className="flex-shrink-0" />
+            <p>Drag to move, pinch to zoom.</p>
+          </li>
+          <li className="flex gap-2">
+            <IconMapPin className="flex-shrink-0" />
+            <p>Tap a pin to view the place's details.</p>
+          </li>
+          <li className="flex gap-2">
+            <IconHandClick className="flex-shrink-0" />
+            <p>
+              Browse or search for places below. Tap on a place to view it on
+              the map.
+            </p>
+          </li>
+        </ul>
+      ) : (
+        <ul className="flex flex-col gap-2 text-sm">
+          <li className="flex gap-2">
+            <IconArrowsMove className="flex-shrink-0" />
+            <p>
+              Drag or use arrow keys to move, scroll or use - + keys to zoom.
+            </p>
+          </li>
+          <li className="flex gap-2">
+            <IconMapPin className="flex-shrink-0" />
+            <p>Click a pin to view the place's details.</p>
+          </li>
+          <li className="flex gap-2">
+            <IconClick className="flex-shrink-0" />
+            <p>
+              Browse or search for places below. Click on a place to view it on
+              the map.
+            </p>
+          </li>
+        </ul>
+      )}
       <Button
         className="focus:ring-offset-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-offset-gray-800"
         onClick={() => setWelcomeMessageDismissed(true)}
